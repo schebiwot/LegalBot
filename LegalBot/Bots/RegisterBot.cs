70820a93ef6912a8fb75fec45b49370cd51c0bfc
schebiwot <s.chebiwot@bigmetal.me>
2020-05-19 13:46:18 +0300

    registering user

---
+++ LegalBot/Bots/RegisterBot.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using LegalBot.Services;
+using System;
+using LegalBot.Helpers;
+
+
+namespace LegalBot.Bots
+{
+    public class RegisterBot<T> : ActivityHandler where T : Dialog
+    {
+        // / Variables 
+        protected readonly Dialog _dialog;
+        protected readonly BotStateService _botStateService;
+        protected readonly ILogger _logger;
+        public RegisterBot(BotStateService botStateService, T dialog, ILogger<RegisterBot<T>> logger)
+        {
+            _botStateService = botStateService ?? throw new ArgumentNullException(nameof(botStateService));
+            _dialog = dialog ?? throw new ArgumentNullException(nameof(dialog));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+        // called when a bot receives an activity...
+        public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            await base.OnTurnAsync(turnContext, cancellationToken);
+            await _botStateService.UserState.SaveChangesAsync(turnContext, false, cancellationToken);
+            await _botStateService.ConversationState.SaveChangesAsync(turnContext, false, cancellationToken);
+        }
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            // how we log information on our logger...
+            _logger.LogInformation("Running dialog with Message Activity");
+            await _dialog.Run(turnContext, _botStateService.DialogStateAccessor, cancellationToken);
+        }
+    }
+}
--- LegalBot/Startup.cs
+++ LegalBot/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 using LegalBot.Bots;
+using LegalBot.Dialogs;
+using LegalBot.Services;
 
 namespace LegalBot
 {
@@ -32,9 +34,42 @@ namespace LegalBot
             // Create the Bot Framework Adapter with error handling enabled.
             services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
 
+            ConfigureState(services);
+            ConfigureDialogs(services);
+
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
-            services.AddTransient<IBot, EchoBot>();
+            services.AddTransient<IBot, RegisterBot<MainDialog>>();
+
+
+
+           
+            // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+            // How we connect the bot to the system...
+            // create the bot as a transient in the case the ASP controller is expecting IBOT...
+            
+
+
+                    }
+        public void ConfigureDialogs(IServiceCollection services)
+        {
+            services.AddSingleton<MainDialog>();
         }
+
+        public void ConfigureState(IServiceCollection services)
+        {
+            // create the storage we'll be using for user and conversation state (Memory is great for testing purposes)
+            services.AddSingleton<IStorage, MemoryStorage>();
+            // create singletons for the user state
+
+            // services.AddSingleton<IStorage>(new AzureBlobStorage(storageAccount,storageContainer));
+            services.AddSingleton<UserState>();
+            // create the conversation state
+            services.AddSingleton<ConversationState>();
+            // create an instance of the state service
+            services.AddSingleton<BotStateService>();
+        }
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
