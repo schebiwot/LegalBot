c0046709a068f73bda6dfa4252f60159c7965bb7
maryanne <mary.jereh@gmail.com>
2020-05-27 17:26:30 +0300

    removed .histroy file and .vscode

---
+++ LegalBot/Dialogs/MainDialog.cs
@@ -0,0 +1,45 @@
+﻿using LegalBot.Services;
+using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LegalBot.Dialogs
+{
+    public class MainDialog : ComponentDialog
+    {
+        private readonly BotStateService _botStateService;
+        public MainDialog(BotStateService botStateService)
+        {
+            _botStateService = botStateService ?? throw new ArgumentNullException(nameof(botStateService));
+            InitializeWaterfallDialog();
+        }
+        private void InitializeWaterfallDialog()
+        {
+            var waterfallSteps = new WaterfallStep[]
+            {
+                InitialSetupAsync,
+                FinalStepAsync
+            };
+            // which dialog we are going to call...
+
+            AddDialog(new EnglishDetailsDialog($"{nameof(MainDialog)}.englishDialog", _botStateService));
+            AddDialog(new WaterfallDialog($"{nameof(MainDialog)}.mainFlow", waterfallSteps));
+            InitialDialogId = $"{nameof(MainDialog)}.mainFlow";
+        }
+        private async Task<DialogTurnResult> InitialSetupAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+           
+                return await stepContext.BeginDialogAsync($"{nameof(MainDialog)}.englishDialog", null, cancellationToken);
+            
+             
+        }
+        private async Task<DialogTurnResult> FinalStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            return await stepContext.EndDialogAsync(null, cancellationToken);
+        }
+    }
+
+}
---
+++ LegalBot/Startup.cs
@@ -0,0 +1,93 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.6.2
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using LegalBot.Bots;
+using LegalBot.Dialogs;
+using LegalBot.Services;
+
+namespace LegalBot
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Create the Bot Framework Adapter with error handling enabled.
+            services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
+
+            ConfigureState(services);
+            ConfigureDialogs(services);
+
+            // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+            services.AddTransient<IBot, RegisterBot<MainDialog>>();
+
+
+
+           
+            // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+            // How we connect the bot to the system...
+            // create the bot as a transient in the case the ASP controller is expecting IBOT...
+            
+
+
+                    }
+        public void ConfigureDialogs(IServiceCollection services)
+        {
+            services.AddSingleton<MainDialog>();
+        }
+
+        public void ConfigureState(IServiceCollection services)
+        {
+            // create the storage we'll be using for user and conversation state (Memory is great for testing purposes)
+            services.AddSingleton<IStorage, MemoryStorage>();
+            // create singletons for the user state
+
+            // services.AddSingleton<IStorage>(new AzureBlobStorage(storageAccount,storageContainer));
+            services.AddSingleton<UserState>();
+            // create the conversation state
+            services.AddSingleton<ConversationState>();
+            // create an instance of the state service
+            services.AddSingleton<BotStateService>();
+        }
+
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseWebSockets();
+            //app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
