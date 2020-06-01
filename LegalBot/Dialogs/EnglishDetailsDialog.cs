09e1cd9f3b2b343eeca83de46724939bd9dda0b9
schebiwot <s.chebiwot@bigmetal.me>
2020-06-01 22:14:06 +0300

    Telegram share using adaptive card

--- LegalBot/Dialogs/EnglishDetailsDialog.cs
+++ LegalBot/Dialogs/EnglishDetailsDialog.cs
@@ -13,7 +13,9 @@ using System.Threading.Tasks;
 using LegalBot.Models;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
-
+using AdaptiveCards;
+using Microsoft.Bot.Schema;
+using System.Runtime.InteropServices.WindowsRuntime;
 
 namespace LegalBot.Dialogs
 {   
@@ -43,8 +45,11 @@ namespace LegalBot.Dialogs
                 CountyStepAsync,
                 SubCountyStepAsync,
                 WardStepAsync,
-                ConfirmationStepAsync,
-                
+                //ConfirmationStepAsync,
+                SelectedMenuAsync,
+                ChooseMenuAsync,
+                ChooseActionAsync,
+           
             };
 
             AddDialog(new WaterfallDialog($"{nameof(EnglishDetailsDialog)}.mainFlow", waterfallSteps));
@@ -53,8 +58,12 @@ namespace LegalBot.Dialogs
             AddDialog(new NumberPrompt<int>($"{nameof(EnglishDetailsDialog)}.county"));
             AddDialog(new NumberPrompt<int>($"{nameof(EnglishDetailsDialog)}.subcounty"));
             AddDialog(new NumberPrompt<int>($"{nameof(EnglishDetailsDialog)}.ward"));
-            AddDialog(new TextPrompt($"{nameof(EnglishDetailsDialog)}.menu"));
-            // AddDialog(new TextPrompt($"{nameof(EnglishDetailsDialog)}.location"));
+            AddDialog(new ChoicePrompt($"{nameof(EnglishDetailsDialog)}.menu"));
+            AddDialog(new ChoicePrompt($"{nameof(EnglishDetailsDialog)}.chooseMenu"));
+            AddDialog(new ChoicePrompt($"{nameof(EnglishDetailsDialog)}.chooseAction"));
+            
+
+
             // set the starting dialog 
             InitialDialogId = $"{nameof(EnglishDetailsDialog)}.mainFlow";
         }
@@ -266,38 +275,157 @@ namespace LegalBot.Dialogs
             }
         }
 
-        //value we get back from the choice prompt
-        private async Task<DialogTurnResult> ConfirmationStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken) {
+        private async Task<DialogTurnResult> SelectedMenuAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
             stepContext.Values["ward"] = (int)stepContext.Result;
-            
-            var userDetails= await _botStateService.UserDetailsAccessor.GetAsync(stepContext.Context, () => new UserDetails(), cancellationToken);
 
-                 userDetails.Language = (string) stepContext.Values["preferredLanguage"];
-                 userDetails.FullName = (string) stepContext.Values["name"];
-                 userDetails.County = county;
-                 userDetails.SubCounty =sub_county;
-                 
-            // Show the summary to the user 
-            if((string) stepContext.Values["preferredLanguage"] == "English"){
-            await stepContext.Context.SendActivityAsync(MessageFactory.Text($" Congratulations {userDetails.FullName}! You are now" +
-                $" registered to use our service.. Please choose (1. MAIN MENU) to continue using the service,"),cancellationToken);
-            }
-            else{
-                await stepContext.Context.SendActivityAsync(MessageFactory.Text($" Karibu {userDetails.FullName}! tumekusajili" +
-                $" Katika service yetu.. Tafadhali chagua (1. MAIN MENU) ndio uweze kuendelea kutumia service zetu...,"),cancellationToken);
-            }
-            
-            await _botStateService.UserDetailsAccessor.SetAsync(stepContext.Context, userDetails);
+            var promptOptions = new PromptOptions
+            {
+                Prompt = MessageFactory.Text
+                (
+                 "Hello, Please choose to continue with our service or go back (1. OTHER OPTIONS),  (2. GO BACK)"),
+                Choices = ChoiceFactory.ToChoices(new List<string> { "OTHER OPTIONS", "Go BACK" }),
+            };
+
+
+            return await stepContext.PromptAsync($"{nameof(EnglishDetailsDialog)}.menu", promptOptions, cancellationToken);
+        }
+        private async Task<DialogTurnResult> ChooseMenuAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            stepContext.Values["menu"] = ((FoundChoice)stepContext.Result).Value;
+
+            if ((string)stepContext.Values["menu"] == "OTHER OPTIONS")
+            {
+
              
-             // waterfall step always finishes eith the end of the waterfall so here is where it ends
+                var promptOptions = new PromptOptions
+                {
+                    Prompt = MessageFactory.Text("hello ,Welcome back, Please choose our services from the list\n"),
+                    Choices = ChoiceFactory.ToChoices(new List<string> { "INFORMATION", "NEWS", " REFFERAL", "SURVEY", "UPDATE", "SHARE" }),
 
-            return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
+
+
+                };
+
+
+
+
+                return await stepContext.PromptAsync($"{nameof(EnglishDetailsDialog)}.chooseMenu", promptOptions, cancellationToken);
+
             }
+
+            else
+            {
+                var promptOptions = new PromptOptions
+                {
+                    Prompt = MessageFactory.Text(" Go back to the main menu")
+                };
+
+                return await stepContext.PromptAsync($"{nameof(EnglishDetailsDialog)}.chooseMenu", promptOptions, cancellationToken);
+
+            }
+        }
+
+
+        private async Task<DialogTurnResult> ChooseActionAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            stepContext.Values["chooseMenu"] = ((FoundChoice)stepContext.Result).Value;
+
+            if ((string)stepContext.Values["chooseMenu"] == "SHARE")
+            {
+                var choices = new[] { "Share on telegram", "Go back" };
+
+                // Create card
+                var card = new AdaptiveCard(new AdaptiveSchemaVersion(1, 0))
+                {
+                    Actions = choices.Select(choice => new AdaptiveSubmitAction
+                    {
+                        Title = choice,
+                        Data = choice, // This will be a string
+
+                    }).ToList<AdaptiveAction>(),
+                  
+                };
+
+                card.Body.Add(new AdaptiveTextBlock() { Text = "Good Evening" });
+                var promptOptions = new PromptOptions
+                {
+                    Prompt = new Activity
+                    {
+                        Attachments = new List<Attachment>() {
+                        new Attachment()
+                    {
+                ContentType = AdaptiveCard.ContentType,
+                Content=card,
+                //Content = JObject.FromObject(card),
+                    }
+                        }
+                    },
+                    Choices = ChoiceFactory.ToChoices(choices),
+                    Style = ListStyle.None,
+                };
+
+                return await stepContext.PromptAsync($"{nameof(EnglishDetailsDialog)}.chooseAction", promptOptions, cancellationToken);
+            }
+
+
+            else
+            {
+                var promptOptions = new PromptOptions
+                {
+                    Prompt = MessageFactory.Text(" Go back to 444 the main menu")
+                };
+
+                return await stepContext.PromptAsync($"{nameof(EnglishDetailsDialog)}.chooseAction", promptOptions, cancellationToken);
+              
+            }
+           
+        }
+
+
+
+        //value we get back from the choice prompt
+       // private async Task<DialogTurnResult> ConfirmationStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken) {
+        //    stepContext.Values["chooseAction"] = (int)stepContext.Result;
             
-          
+        //    var userDetails= await _botStateService.UserDetailsAccessor.GetAsync(stepContext.Context, () => new UserDetails(), cancellationToken);
+
+        //         userDetails.Language = (string) stepContext.Values["preferredLanguage"];
+        //         userDetails.FullName = (string) stepContext.Values["name"];
+        //         userDetails.County = county;
+        //         userDetails.SubCounty =sub_county;
+        //    //var promptOptions = new PromptOptions
+        //    //{
+        //    //    Prompt = MessageFactory.Text
+        //    //  (
+        //    //   "Hello, Please choose to continue with our service or go back (1. OTHER OPTIONS),  (2. GO BACK)"),
+        //    //    Choices = ChoiceFactory.ToChoices(new List<string> { "OTHER OPTIONS", "Go BACK" }),
+        //    //};
+
+        //    // Show the summary to the user 
+        //    if ((string) stepContext.Values["preferredLanguage"] == "English"){
+        //    await stepContext.Context.SendActivityAsync(
+
+        //        MessageFactory.Text($" Congratulations {userDetails.FullName}! You are now" +
+        //        $" registered to use our service.. Please choose (1. MAIN MENU) to continue using the service,"),cancellationToken);
+
+        //    }
+        //    else{
+        //        await stepContext.Context.SendActivityAsync(MessageFactory.Text($" Karibu {userDetails.FullName}! tumekusajili" +
+        //        $" Katika service yetu.. Tafadhali chagua (1. MAIN MENU) ndio uweze kuendelea kutumia service zetu...,"),cancellationToken);
+        //    }
+            
+        //    await _botStateService.UserDetailsAccessor.SetAsync(stepContext.Context, userDetails);
+
+        //    // waterfall step always finishes eith the end of the waterfall so here is where it ends
+        //    return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
+        //   // return await stepContext.PromptAsync($"{nameof(EnglishDetailsDialog)}.menu", promptOptions, cancellationToken);
+        //}
+
+    
         // our validators...
-        
 
 
-            }
+
+    }
 }
